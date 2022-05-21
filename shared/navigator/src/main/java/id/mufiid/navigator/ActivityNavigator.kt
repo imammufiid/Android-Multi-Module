ca260ef8819171dc8cf8e36b132ffec2b009fe72
imufiid <imufiid98@gmail.com>
2022-05-21 10:52:16 +0700

    Setup sample navigator

---
+++ shared/navigator/src/main/java/id/mufiid/navigator/ActivityNavigator.kt
@@ -0,0 +1,9 @@
+package id.mufiid.navigator
+
+import id.mufiid.navigator.activity.DetailActivityContainer
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class ActivityNavigator: KoinComponent {
+    val DETAIL_ACTIVITY_CONTAINER: DetailActivityContainer by inject()
+}
---
+++ shared/navigator/src/main/java/id/mufiid/navigator/activity/DetailActivityContainer.kt
@@ -0,0 +1,7 @@
+package id.mufiid.navigator.activity
+
+import android.app.Activity
+
+interface DetailActivityContainer {
+    val activityDetailClass: Class<Activity>
+}
