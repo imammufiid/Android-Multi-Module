13923de1be466f937df4bfbf2a60224ad7067b92
imufiid <imufiid98@gmail.com>
2022-05-21 10:53:15 +0700

    Created sample activity

---
+++ features/Detail/src/main/java/id/mufiid/detail/ui/DetailActivity.kt
@@ -0,0 +1,12 @@
+package id.mufiid.detail.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.mufiid.detail.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
