d04500a4e125fd0a4a9857a324bdc4e4f9567f47
imufiid <imufiid98@gmail.com>
2022-05-21 14:54:02 +0700

    Set how to intent navigate with navigator koin

--- app/src/main/java/id/mufiid/koinnavigationmodule/MainActivity.kt
+++ app/src/main/java/id/mufiid/koinnavigationmodule/MainActivity.kt
@@ -1,11 +1,24 @@
 package id.mufiid.koinnavigationmodule
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.mufiid.koinnavigationmodule.databinding.ActivityMainBinding
+import id.mufiid.navigator.ActivityNavigator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_bind.root)
+
+        _bind.btnNavigate.setOnClickListener {
+            val detailClass = ActivityNavigator.DETAIL_ACTIVITY_CONTAINER.activityDetailClass
+            val intent = Intent(this, detailClass)
+
+            startActivity(intent)
+        }
     }
 }
